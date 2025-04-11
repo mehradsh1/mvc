a108e057571bf28ac8c6a2ce6cc43c664b8a45e9
Mehrad <a.mehrno47@gmail.com>
2025-04-11 16:33:02 +0330

    all

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use app\core\application;
+
+$app= new application();
+
+$app->router->get('/', function(){
+    return 'hello world';
+});
+$app->router->get('/contact', function(){
+    return 'Contact';
+});
+
+
+
+$app->run();
